358c2a3987f00fc0741f6f6105ff77606f709b85
Saksham <sakshamjain0920@gmail.com>
2020-01-18 13:03:07 -0500

    Testing

--- thermal.py
+++ thermal.py
@@ -1,7 +1,5 @@
 from math import sqrt, cos
 
-
-
 # Heat transfer within boundary layer as a func. of Mach#,
 # altitude, and angle of attack. Laminar + Turbulent
 # Based on the "Reference Temperature Method of Eckert" where
@@ -14,17 +12,24 @@ from math import sqrt, cos
 # on conical side of vehicle, rest is flat plate.
 # Heat transfer coefficient is calculated iterativelyy until
 # at each time-step.
+
+# Data entry
 h = 0.001 # Global Variable
 constant = {
 "delta":5, # Wall Thickness
 "k":6,       # Thermal Conductivity
 "gamma":5   # Specific Heat Ratio of Flow
 }
+
 # Biot Number Check (for thin wall)
 bi = h*(constant.delta/constant.k)
 if bi >= 0.1:
     print("Wall cannot be modelled as thermally thin")
-# Recovery Temperature
+
+# Atmospheric Property Generation
+
+
+# Recovery Temperature Calculation
 M = 0.8 # Mach Speed to be redefined w/ profile later
 P = 0.147 # Freestream Pressure re-defined later
 q = 0.148 # Dynamic Pressure
@@ -43,5 +48,4 @@ elif M >= 1:
 
 Tl = ToL/  (1+((gamma-1)/2)*(Ml**2))
 Tr = Tl*(1+r*((gamma-1)/2)*(Ml**2))
-
 
