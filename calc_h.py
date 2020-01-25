cf09d79280264478279c91f0c9899d7fd617e82f
Saksham <sakshamjain0920@gmail.com>
2020-01-25 12:28:59 -0500

    Update calc_h.py

--- calc_h.py
+++ calc_h.py
@@ -26,4 +26,3 @@ def calc_h (gamma, aoa, q, P_inf, C_pmax, M_inf, T_localstag, T_wall, x, Re, Pr,
         Nu = 0.33206*sqrt(Re)*((Pr)**(1/3))
     else:
         Nu = 0.02914*(Re**0.8)*(Pr**(1/3))
-    
