39e25c000d3ca86af75736109d1ccb3818e86c81
Saksham <sakshamjain0920@gmail.com>
2020-02-29 13:19:20 -0500

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,31 @@
+def calc_P_local(A_pmax, P_inf, q):
+
+
+def calc_P_infstag(M_inf):
+
+
+def calc_P_localstag_sub(P_local, P_infstag, M_inf):
+
+
+def calc_P_localstag_super(P_infstag, M_inf):
+
+
+def calc_mach_local(P_localstag, P_local):
+
+
+def calc_T_local(T_localstag, mach_local):
+
+
+def calc_T_recov(recov_fact, mach_local):
+
+
+def calc_T_ref(T_local, T_recov):
+
+
+def calc_k_ref(T_ref):
+
+
+def calc_h(Nu, k_ref, dist):
+
+
+def calcTemp():     # Need to add arguments to this
