68804b936b5202bcb9e153eaf9648c1353ad89f8
Saksham <sakshamjain0920@gmail.com>
2020-04-10 16:03:08 -0400

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,7 +1,9 @@
 # ** Define these before running simulation **
 
-# Characteristic Length [m]
+# Dimensions [m]
 x_char = None
+w_fiberglass = None     # Length of fiberglass layer
+w_epoxy = None          # Length of epoxy layer
 # Thermal Conductivity
 k = None
 # Specific Heat Capacity
@@ -19,8 +21,12 @@ gamma = None
 
 def getx_char():
     return x_char
-def getk():
-    return k
+def getk(T_wall):
+    # T vs k data extrapolated from: https://www.engineeringtoolbox.com/fiberglas-insulation-k-values-d_1172.html
+    k_fiberglass = (1.59*(10**(-4)))*T_wall - 0.008
+    
+    k_eff = (k_fiberglass*w_fiberglass + k_epoxy*w_epoxy)/(w_fiberglass + w_epoxy)
+    return k_eff
 def getc_p():
     return c_p
 def getAlt():
