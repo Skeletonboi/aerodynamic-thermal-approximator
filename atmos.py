6948309092630d70604f1a25a79fe33e796f13dd
eqd11 <ericdai1130@gmail.com>
2020-04-11 16:08:08 -0400

    proper thingies

--- atmos.py
+++ atmos.py
@@ -59,12 +59,7 @@ class Atmos:
 	def getT_static(self, alt):	#TODO: Add static temp data wrt altitude and extrapolate from that for this fnc
 		return T_static
 
-	def getT_localstag(self,alt):
-		Mach = getSpeed()
-		gamma = get_gamma()
-		return 1
-	
-	def getT_localstag(self, alt, speed,c):
+	def getT_localstag(self, alt, speed, c):
 		Temp = get_temp() #need to have a function that gets the temp at certain altitude
 		T_localstag = Temp + speed**2/(c*2)
 		return T_localstag
